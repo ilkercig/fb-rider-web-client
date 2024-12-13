f7ba27cf03d841d6aafced27e043f16cc77623a2
ilkercigdemal <ilkercigdemal@gmail.com>
2024-12-13 17:40:45 +0300

    attempt to fix login redirection

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -4,17 +4,29 @@ import { CircularProgress, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 
 const ProtectedRoute: React.FC = () => {
-    const { data: isAuth, isLoading } = useQuery({
-        queryKey: ["checkAuthentication"],
-        queryFn: checkAuthentication,
-    })
+  const {
+    data: isAuth,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["checkAuthentication"],
+    queryFn: checkAuthentication,
+  });
 
-    // While checking authentication, you can render a loading spinner or similar
-    if (isLoading) {
-        return <><CircularProgress /><Typography variant="h6">Loading...</Typography></>
-    }
+  // While checking authentication, you can render a loading spinner or similar
+  if (isLoading) {
+    return (
+      <>
+        <CircularProgress />
+        <Typography variant="h6">Loading...</Typography>
+      </>
+    );
+  }
+  if (isError || !isAuth) {
+    return <Navigate to="/login" replace />;
+  }
 
-    return isAuth ? <Outlet /> : <Navigate to="/login" />;
+  return <Outlet />;
 };
 
 export default ProtectedRoute;
